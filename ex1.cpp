1f9ab31fac493708b6de21db8ff024eef06cd772
ty-hongjy <hongjy1@163.com>
2017-09-20 14:50:31 +0800

    0920

---
+++ ex1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+int main()
+{
+	cout<<"hello world"<<endl<<endl;
+	
+	puts("hello world\n");
+	
+	printf("hello world\n");
+	return 0;
+}
