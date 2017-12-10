5b851f55b131abf6813ddfbf7e3db2e8b6ff2446
ty-hongjy <hongjy1@163.com>
2017-12-10 21:34:15 +0800

    1210

---
+++ noip/cpp2017-01.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+int func(int a,int b,int c,int d){
+  if((c%a==0)&&(d%b==0)){
+	    cout<< c+d<<": "<<c/a<<" , "<<d/b<<endl;
+	    return 0;}
+	  else if((d-a)<0){
+	    cout<< "*"<<c+d<<": "<<c/a<<" , "<<d/b<<endl;
+	    return -1;
+	  }
+	  else
+	    func(a,b,c+a,d-a);
+	}
+
+int main()
+{
+	int i;
+	cout<<"hello world"<<endl<<endl;
+	
+	puts("start\n");
+	for(i=1;i<=101;i++){
+		func(3,5,0,i);
+	}
+	cout<<"End"<<endl<<endl;
+	return 0;
+}
